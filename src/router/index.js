309b43bcebd2dc4a54f64e9f34016ba46d5061a2
xiaoyu-11 <53254610+xiaoyu-11@users.noreply.github.com>
2020-04-01 18:31:26 +0800

    初步完成自适应配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import home from '@/views/home'
 
 Vue.use(Router)
 
@@ -8,6 +10,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
     }
---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+      <div class="main-top">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.main{
+    font-size: 13px;
+	&-top{
+		width: 100%;
+		height: 390px;
+		background-color: #FF4A2B;
+	}
+}
+</style>
